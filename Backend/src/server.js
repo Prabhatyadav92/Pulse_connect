38aebd96b084fe73ac34402e677ee40ee7658f78
Prabhat Yadav <prabhatyadav@Prabhats-MacBook-Air.local>
2026-03-15 11:18:35 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,23 +1,30 @@
 // const express=require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import path from 'path';
+
+
 dotenv.config();
 
 
 const app=express();
+
+const __dirname=path.resolve();
+
 const PORT=process.env.PORT || 3000;
 
+app.use("api/auth",authRoutes);
+app.use("api/messages",messageRoutes);
+// make ready for deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+    });
 
-app.get('/api/auth/signup',(req,res)=> {
-    res.json({message: 'Signup endpoint'});
-});
+}
 
-app.get('/api/auth/login',(req,res)=> {
-    res.json({message: 'Login endpoint'});
-});
-
-app.get('/api/auth/logout',(req,res)=> {
-    res.json({message: 'Logout endpoint'});
-});
 
 app.listen (PORT,()=> console.log("Server is running on port :"+PORT));
