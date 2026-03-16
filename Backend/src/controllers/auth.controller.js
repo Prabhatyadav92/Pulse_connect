3cd8e1f37fd5930817defeb54cf85f465f62be69
Prabhat Yadav <prabhatyadav@gmail.com>
2026-03-17 04:27:04 +0530

    signup section code

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+import { generateToken } from '../../lib/utils.js';
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req,res)=> {
+
+    const { fullName, email, password } = req.body;
+
+    try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"all fields are required"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message:"password should be 6 characters"});
+        }
+
+        const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message:"invalid email format"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({message:"email already exists"});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if(newUser){
+
+            await newUser.save();
+
+            generateToken(newUser._id,res);
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic
+            });
+
+        }else{
+            res.status(400).json({message:"failed to create user"});
+        }
+
+    }catch(err){
+
+        console.log("error in signup controller:",err);
+        res.status(500).json({message:"internal server error"});
+
+    }
+};
