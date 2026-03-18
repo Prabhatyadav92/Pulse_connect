007a1c3b44f9be2289fad372bd9ea02b319c3d68
Prabhat Yadav <prabhatyadav@gmail.com>
2026-03-18 17:31:09 +0530

    login and logout functionality added

--- Backend/lib/utils.js
+++ Backend/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken= (userId,res)=> {
     const token= jwt.sign({userId},ENV.JWT_SECRET,{
         expiresIn:"7d"
     });
-    res.cookie("token",token,{
+    res.cookie("JWT",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
         sameSite:"strict",
